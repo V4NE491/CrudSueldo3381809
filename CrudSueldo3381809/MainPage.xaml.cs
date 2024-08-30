5ff6461ba518f08ba0df3bfdee1b1fd9bc38cfeb
MINEDUCYT <MINEDUCYT@DESKTOP-GLJR0B3>
2024-08-29 22:45:39 -0600

    Agregar archivos de proyecto.

---
+++ CrudSueldo3381809/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace CrudSueldo3381809
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Obtener valores del Entry
+            int horasTrabajadas = int.Parse(entryHoras.Text);
+            decimal pagoPorHora = decimal.Parse(entryPago.Text);
+
+            // Crear un objeto Trabajador y calcular el sueldo
+            Trabajador trabajador = new Trabajador
+            {
+                HorasTrabajadas = horasTrabajadas,
+                PagoPorHora = pagoPorHora
+            };
+
+            decimal sueldoSemanal = trabajador.CalcularSueldoSemanal();
+            // Mostrar el resultado
+            labelSueldo.Text = $"Sueldo Semanal: ${sueldoSemanal}";
+        }
+    }
+
+}
---
+++ CrudSueldo3381809/Trabajador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudSueldo3381809
+{
+    public class Trabajador
+    {
+        public int HorasTrabajadas { get; set; }
+        public decimal PagoPorHora { get; set; }
+
+        public decimal CalcularSueldoSemanal()
+        {
+            return HorasTrabajadas * PagoPorHora;
+        }
+    }
+}
